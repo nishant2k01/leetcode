97aed4d7d3b4840a806288990e5f918624f7239e
Nishant Katara <54469275+nishant2k01@users.noreply.github.com>
2024-01-24 22:37:20 +0530

    Time: 246 ms (89.81%), Space: 176.9 MB (99.73%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        if (root == NULL) return 0;
+        
+        // update the count
+        count[root->val]++;
+
+        // check the pseudo-palindromic for the leaf node
+        int value = 0;
+        if ((root->left == NULL) && (root->right == NULL)) {
+            if (check()) value = 1;
+            else value = 0;
+        }
+        else {
+            value = pseudoPalindromicPaths(root->left) + pseudoPalindromicPaths(root->right);
+        }
+
+        // revert the count
+        count[root->val]--;
+        
+        return value;
+    }
+    
+private:
+    int count[10] = {0};
+    bool check(void) {
+        int oddCount = 0;
+        for (int i = 1; i <= 9; i++) {
+            if ((count[i] & 1) == 1) oddCount++;
+        }
+        return (oddCount <= 1);
+    }
+};
